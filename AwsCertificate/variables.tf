cd7095432d9ab9d3529c2c853db856d14776cced
monkee <lyndon.swan@maxkelsen.com>
2021-03-23 12:07:19 +1000

    Add AwsCertificate

--- AwsCertificate/variables.tf
+++ AwsCertificate/variables.tf
@@ -1,17 +1,44 @@
-variable "aws_region" {}
+variable "aws_region" {
+  type = string
+  description = "The AWS Region to deploy resources"
+}
 
-variable "environment" {}
+variable "environment" {
+  type = string
+  description = "The deployment environment"
+}
 
-variable "parent_domain_suffix" {}
+variable "parent_domain_suffix" {
+  type = string
+  description = "The parent domain suffix for the certificate"
+}
 
-variable "parent_api_zone_id" {}
+variable "parent_api_zone_id" {
+  type = string
+  description = "The parent domain zone id"
+}
 
-variable "domain_prefix" {}
+variable "domain_prefix" {
+  type = string
+  description = "The prefix for the domain"
+}
 
-variable "api_alb_zone_id" {}
+variable "api_alb_zone_id" {
+  type = string
+  description = "The ALB zone id"
+}
 
-variable "api_alb_dns_name" {}
+variable "api_alb_dns_name" {
+  type = string
+  description = "The ALB DNS Name"
+}
 
-variable "api_alb_arn" {}
+variable "api_alb_arn" {
+  type = string
+  description = "The ALB ARN"
+}
 
-variable "api_fargate_lb_target_group_arn" {}
+variable "api_fargate_lb_target_group_arn" {
+  type = string
+  description = "The ALB target group ARN"
+}
