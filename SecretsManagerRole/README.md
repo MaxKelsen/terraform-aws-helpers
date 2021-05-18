6f9d67662350e23cdcef00e17e17b6d15eeeab0d
Matthew Rose <matthew.rose@maxkelsen.com>
2021-05-18 12:58:15 +1000

    Add and run precommit hooks over current repo, also adds local development section to main README as well as terraform-docs markers to module READMEs

--- SecretsManagerRole/README.md
+++ SecretsManagerRole/README.md
@@ -13,6 +13,7 @@ The secret should be stored the following way, in the secrets manager (plaintext
             "SecretAccessKey": "######################"
         }
 
+<!-- BEGINNING OF PRE-COMMIT-TERRAFORM DOCS HOOK -->
 ## Requirements
 
 No requirements.
@@ -21,32 +22,33 @@ No requirements.
 
 | Name | Version |
 |------|---------|
-| aws | n/a |
+| <a name="provider_aws"></a> [aws](#provider\_aws) | n/a |
 
 ## Modules
 
-No Modules.
+No modules.
 
 ## Resources
 
-| Name |
-|------|
-| [aws_secretsmanager_secret](https://registry.terraform.io/providers/hashicorp/aws/latest/docs/data-sources/secretsmanager_secret) |
-| [aws_secretsmanager_secret_version](https://registry.terraform.io/providers/hashicorp/aws/latest/docs/data-sources/secretsmanager_secret_version) |
+| Name | Type |
+|------|------|
+| [aws_secretsmanager_secret.role](https://registry.terraform.io/providers/hashicorp/aws/latest/docs/data-sources/secretsmanager_secret) | data source |
+| [aws_secretsmanager_secret_version.role](https://registry.terraform.io/providers/hashicorp/aws/latest/docs/data-sources/secretsmanager_secret_version) | data source |
 
 ## Inputs
 
 | Name | Description | Type | Default | Required |
 |------|-------------|------|---------|:--------:|
-| aws\_region | A valid region where secretsmanager holds the secrets | `string` | n/a | yes |
-| secret\_name | The specific secret name | `string` | n/a | yes |
+| <a name="input_aws_region"></a> [aws\_region](#input\_aws\_region) | A valid region where secretsmanager holds the secrets | `string` | n/a | yes |
+| <a name="input_secret_name"></a> [secret\_name](#input\_secret\_name) | The specific secret name | `string` | n/a | yes |
 
 ## Outputs
 
 | Name | Description |
 |------|-------------|
-| role\_access\_key | JSON decoded AWS Access Key |
-| role\_secret\_access\_key | JSON decoded AWS Secret Access Key |
+| <a name="output_role_access_key"></a> [role\_access\_key](#output\_role\_access\_key) | JSON decoded AWS Access Key |
+| <a name="output_role_secret_access_key"></a> [role\_secret\_access\_key](#output\_role\_secret\_access\_key) | JSON decoded AWS Secret Access Key |
+<!-- END OF PRE-COMMIT-TERRAFORM DOCS HOOK -->
 
 ### Changes
 + refactored to be more generic
--- SqsWithDlq/main.tf
+++ SqsWithDlq/main.tf
@@ -1,39 +1,38 @@
 resource "aws_sqs_queue" "queue_deadletter" {
-  for_each = var.sqs_queues
-  name = format("%s-dlq-%s-%s", each.key, var.aws_region, var.environment)
-  fifo_queue = each.value["fifo_queue"]
-  delay_seconds = each.value["delay_seconds"]
-  max_message_size = each.value["max_message_size"]
-  message_retention_seconds = each.value["max_message_size"]
-  policy = ""
-  receive_wait_time_seconds = each.value["receive_wait_time_seconds"]
-  visibility_timeout_seconds = each.value["visibility_timeout_seconds"]
-  kms_master_key_id = each.value["kms_master_key_id"]
+  for_each                          = var.sqs_queues
+  name                              = format("%s-dlq-%s-%s", each.key, var.aws_region, var.environment)
+  fifo_queue                        = each.value["fifo_queue"]
+  delay_seconds                     = each.value["delay_seconds"]
+  max_message_size                  = each.value["max_message_size"]
+  message_retention_seconds         = each.value["max_message_size"]
+  policy                            = ""
+  receive_wait_time_seconds         = each.value["receive_wait_time_seconds"]
+  visibility_timeout_seconds        = each.value["visibility_timeout_seconds"]
+  kms_master_key_id                 = each.value["kms_master_key_id"]
   kms_data_key_reuse_period_seconds = each.value["kms_data_key_reuse_period_seconds"]
   tags = {
     environment = var.environment
-    deployment = "terraform"
+    deployment  = "terraform"
   }
 }
 
 resource "aws_sqs_queue" "queue" {
-  depends_on = [
-    aws_sqs_queue.queue_deadletter]
-  for_each = var.sqs_queues
-  name = format("%s-%s-%s", each.key, var.aws_region, var.environment)
-  fifo_queue = each.value["fifo_queue"]
-  delay_seconds = each.value["delay_seconds"]
-  max_message_size = each.value["max_message_size"]
+  depends_on                = [aws_sqs_queue.queue_deadletter]
+  for_each                  = var.sqs_queues
+  name                      = format("%s-%s-%s", each.key, var.aws_region, var.environment)
+  fifo_queue                = each.value["fifo_queue"]
+  delay_seconds             = each.value["delay_seconds"]
+  max_message_size          = each.value["max_message_size"]
   message_retention_seconds = each.value["max_message_size"]
   receive_wait_time_seconds = each.value["receive_wait_time_seconds"]
   redrive_policy = jsonencode({
     deadLetterTargetArn = aws_sqs_queue.queue_deadletter[each.key].arn
-    maxReceiveCount = 5
+    maxReceiveCount     = 5
   })
-  kms_master_key_id = each.value["kms_master_key_id"]
+  kms_master_key_id                 = each.value["kms_master_key_id"]
   kms_data_key_reuse_period_seconds = each.value["kms_data_key_reuse_period_seconds"]
   tags = {
     environment = var.environment
-    deployment = "terraform"
+    deployment  = "terraform"
   }
 }
